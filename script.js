1138e889064c60c1341acbcfcbc571f9a2524bfe
Jade S <thedavidsoto@gmail.com>
2024-11-26 18:50:33 +0800

    Add js file with event listener to dialog button and dialog->close

---
+++ script.js
@@ -0,0 +1,38 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.status = function(isRead) {
+        if (this.isRead) {
+            return "already read";
+        } else {
+            return "not read yet";
+        }
+    }
+
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status(this.isRead)}`;
+    }
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', false);
+console.log(theHobbit.info());
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+showButton.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+closeButton.addEventListener("click", () => {
+    dialog.close();
+})
