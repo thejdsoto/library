6ee7ef52943d298bdbfee36c713c69ea1c0c3f56
Jade S <thedavidsoto@gmail.com>
2024-11-27 18:34:06 +0800

    Clean up Book object, move tbody variable to global scope, and add submitBookBtn event wherein user can input and display input

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
+const submitBookBtn = document.querySelector(".submit-book");
+const tbody = document.querySelector("tbody");
 const myLibrary = [
     {
         title: "Random book",
@@ -9,32 +11,19 @@ const myLibrary = [
     }
 ];
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead;
-    this.status = function(isRead) {
-        if (this.isRead) {
-            return "already read";
-        } else {
-            return "not read yet";
-        }
-    }
-
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status(this.isRead)}`;
-    }
 }
 
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
 
 function showData() {
     for (let i=0; i<myLibrary.length;i++) {
-        const tr = document.createElement("tr");
-        const tbody = document.querySelector("tbody");
+        const tr = document.createElement("tr"); 
         tbody.appendChild(tr);
         let ele = myLibrary[i];
     
@@ -55,3 +44,15 @@ showButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     dialog.close();
 })
+
+submitBookBtn.addEventListener("click", (e) => {
+    let title = document.querySelector(".title-input");
+    let author = document.querySelector(".author-input");
+    let pages = document.querySelector(".pages-input");
+
+    let book = new Book(title.value, author.value, pages.value);
+    addBookToLibrary(book);
+    tbody.innerHTML = '';
+    showData();
+    e.preventDefault();
+});
