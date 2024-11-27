d99c095580b1c7fc74c5c4e8b58c7f27a5676493
Jade S <thedavidsoto@gmail.com>
2024-11-27 17:49:58 +0800

    Add for loop that will display the data in myLibrary array to html DOM

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "Random book",
+        author: "Authorify",
+        page: "3921039120",
+    }
+];
 
 function Book(title, author, pages, isRead) {
     this.title = title;
@@ -22,17 +28,32 @@ function addBookToLibrary() {
     // do stuff here
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', false);
-console.log(theHobbit.info());
+// const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', false);
+// console.log(theHobbit.info());
 
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog + button");
-const closeButton = document.querySelector("dialog button");
+// const dialog = document.querySelector("dialog");
+// const showButton = document.querySelector("dialog + button");
+// const closeButton = document.querySelector("dialog button");
 
-showButton.addEventListener("click", () => {
-    dialog.showModal();
-})
+// showButton.addEventListener("click", () => {
+//     dialog.showModal();
+// })
 
-closeButton.addEventListener("click", () => {
-    dialog.close();
-})
+// closeButton.addEventListener("click", () => {
+//     dialog.close();
+// })
+
+for (let i=0; i<myLibrary.length;i++) {
+    const tr = document.createElement("tr");
+    const tbody = document.querySelector("tbody");
+    tbody.appendChild(tr);
+    let ele = myLibrary[i];
+
+    for (let key in ele) {
+        const td = document.createElement("td");
+        td.innerText = ele[key];
+        tr.appendChild(td);
+    }
+}
+
+// console.log(myLibrary);
